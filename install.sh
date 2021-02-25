2fdb7a0da9fa9f20b9f643eeecfd40874cc7f73a
hspil <44721499+hspil@users.noreply.github.com>
2021-02-24 19:23:31 -0600

    install script ensures .conf file exists

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
-mkdir ~/.config/prime-manager
+mkdir -p ~/.config/prime-manager
+touch ~/.config/prime-manager/prime-manager.conf
 
 echo "#nvidia prime manager aliases" >> ~/.bashrc
 echo "source ~/.config/prime-manager/prime-managerrc" >> ~/.bashrc
